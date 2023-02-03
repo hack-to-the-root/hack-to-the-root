93fc1ec71237633fdadf15d17bd542c3986a9b71
Bernd Stellwag <burned@zerties.org>
2023-02-03 22:01:17 +0100

    add yaml parser addon and initial code for loading hacking config

--- scenes/hacking/qa.gd
+++ scenes/hacking/qa.gd
@@ -11,7 +11,17 @@ var answer
 func _ready():
 	question = get_node("question")
 	answer = get_node("answer")
-
+	
+	var yaml = preload("res://addons/godot-yaml/gdyaml.gdns").new()
+	var file = File.new()
+	file.open("res://scenes/hacking/config.yaml", File.READ)
+	var config = file.get_as_text()
+	file.close()
+	config = yaml.parse(config).result
+	for line in config:
+		for question in line:
+			print(question)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
