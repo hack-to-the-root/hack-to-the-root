d0f66d7caa240deb57808754162cde2954a5bb66
Bernd Stellwag <burned@zerties.org>
2023-02-03 19:36:08 +0100

    add first prototype

---
+++ qa_input.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var question
+var answer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	question = get_node("question")
+	answer = get_node("answer")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if answer.text == "42":
+		question.text = "Got it!"
