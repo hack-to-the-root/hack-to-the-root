0f7d0729c78ced01be386e6e454e980796322694
Bernd Stellwag <burned@zerties.org>
2023-02-03 20:41:44 +0100

    add script to build locally and configuration to build via ci

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf dist/
+mkdir -p dist
+
+echo 'Building for web ...'
+godot --no-window --export "HTML5" dist/index.html
+echo 'done.'
+
+echo 'Building for linux ...'
+godot --no-window --export 'Linux/X11' dist/hack-to-the-root.x86_64
+chmod a+x dist/hack-to-the-root.x86_64
+echo 'done.'
+
+echo 'done.'
