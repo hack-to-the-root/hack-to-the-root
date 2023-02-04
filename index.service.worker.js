def2fbd9eb886a8ec449fcce763a1e43b288648b
j-brn <me@bricker.io>
2023-02-04 17:38:17 +0000

    Deploying to gh-pages from @ hack-to-the-root/hack-to-the-root@bd7df414a8b9ca52c3e48399990897a179aab42b 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1675531097|5963173";
+const CACHE_VERSION = "1675532250|4929869";
 const CACHE_PREFIX = "h4ck 2 the /root-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
