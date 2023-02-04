8d8dc7f8328871f36519359df0703e4dad1aa172
burned42 <burned@zerties.org>
2023-02-04 13:41:23 +0000

    Deploying to gh-pages from @ hack-to-the-root/hack-to-the-root@3e6dd357060d7579142f82a2c7dfba6a202eae8c 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1675517193|3197765";
+const CACHE_VERSION = "1675518051|3257670";
 const CACHE_PREFIX = "h4ck 2 the /root-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
