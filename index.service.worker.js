b3178facc642a5db083d75998866c388affa530f
EduardBaer <EduardBaer@users.noreply.github.com>
2023-02-04 10:42:24 +0000

    Deploying to gh-pages from @ hack-to-the-root/hack-to-the-root@854b03a23ae91338a7f627c11d0d3a81e36f7b19 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1675506729|2839705";
+const CACHE_VERSION = "1675507325|2896868";
 const CACHE_PREFIX = "h4ck 2 the /root-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
