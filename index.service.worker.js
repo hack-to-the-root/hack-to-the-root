b8005a9c08d0f094edff98a47985830a43fbac47
Hayajiro <ciapa@ciapa.tech>
2023-02-04 15:46:36 +0000

    Deploying to gh-pages from @ hack-to-the-root/hack-to-the-root@20de76bdd65bd802dfde68296170bc1117f5179a 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1675525222|3791685";
+const CACHE_VERSION = "1675525558|4569324";
 const CACHE_PREFIX = "h4ck 2 the /root-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
